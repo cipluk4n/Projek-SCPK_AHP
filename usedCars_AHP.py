cf1611b75f01b49e05b5ac521c90e17bc96cf8b8
cipluk4n <asiilrabbani@gmail.com>
2026-05-24 18:43:14 +0700

    menambahkan poin-poin perhitungannya

--- usedCars_AHP.py
+++ usedCars_AHP.py
@@ -9,9 +9,9 @@ with tab1:
     st.subheader("Kelompok 15 ~ AHP", text_alignment="center")
     # st.subheader("Pemilihan Mobil Bekas Menggunakan AHP")
     with st.container(border=True):  
-        st.write("123240099 - fas")
-        st.write("123240099 - fas")
-        st.write("123240099 - fas")
+        st.write("123240099 - Rabbani")
+        st.write("123240099 - Desta")
+        st.write("123240099 - Dimas")
 with tab2:
     st.write("Pengertian Singkkat Metode AHP")
 with tab3:
@@ -19,9 +19,22 @@ with tab3:
     df = pd.read_csv('used_cars.csv')
     st.dataframe(df)
 with tab4:
-    st.write("")
+    with st.expander("Evaluasi Bobot Kriteria"):
+        st.text("Matriks Perbandingan Berpasangan Kriteria")
+        st.text("Normalisasi dan Perhitungan Bobot")
+        st.text("Matriks Perbandingan Setelah Normalisasi")
+        st.text("Bobot Kriteria")
+        st.text("Uji Validitas Bobot")
+    with st.expander("Evaluasi Alternatif Tiap Kriteria"):
+        st.text("Kriteria Satu")
+        st.text("Kriteria Dua")
+        st.text("Kriteria ...")
+    with st.expander("Perangkingan Akhir"):
+        st.text("Matriks Rekap Bobot ALternatif (W_total)")
+        st.text("Bobot Akhir Tiap Alternatif")
     if st.button("Mulai Perhitungan"):
         st.write("nanti hasilnya ditaruh di tab hasil")    
 with tab5:
     st.write("diisi hasilnya")
+    st.text("Alternatif terbaik")
 
