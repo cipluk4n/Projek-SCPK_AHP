221700b33a39b0bdce5f88ed6dccabbe8b7057c1
local <123240099@student.upnyk.ac.id>
2026-08-25 11:30:06 +0700

    test-nama-config-local

--- usedCars_AHP.py
+++ usedCars_AHP.py
@@ -4,6 +4,7 @@ import pandas as pd
 import streamlit as st
 import time
 
+#test nama config local
 
 st.set_page_config(page_icon='🗿', page_title='SPK Mobil Bekas | AHP', layout='wide')
 st.title('Pemilihan Mobil Bekas dengan Metode AHP', text_alignment='center')
